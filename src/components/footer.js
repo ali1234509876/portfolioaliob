1767d8ce12f191aff345ba3b2704acebb39ffea5
alii <157791316+ali1234509876@users.noreply.github.com>
2026-02-12 07:15:41 +0300

    modified:   src/components/footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <a href="https://github.com/ali1234509876">
+          <div>Designed &amp; Built by Ali Bamaga</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
